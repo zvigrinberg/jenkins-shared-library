c8b7661235fde0b1e6b829e09e44f62d8f129885
Zvi Grinberg <zgrinber@redhat.com>
2022-08-18 12:32:05 +0300

    add test function to helm.groovy

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -1,4 +1,10 @@
+def test(String releaseName,String releaseNamespace)
+{
+    sh "helm test ${releaseName} -n ${releaseNamespace}"
+}
+
 def uninstall(String releaseName,String releaseNamespace)
 {
     sh "helm uninstall ${releaseName} -n ${releaseNamespace}"
 }
+
