d9728bbef2a0943466b532e7523531c5073ecba9
Zvi Grinberg <zgrinber@redhat.com>
2022-08-25 09:40:50 +0300

    add new function waitForDeployment

--- vars/oc.groovy
+++ vars/oc.groovy
@@ -9,3 +9,9 @@ def createPullSecret(String secretName , String username, String password, Strin
 {
     sh "oc create secret docker-registry ${secretName} --docker-server=${server}    --docker-username=${username}    --docker-password=${password} -n ${namespace}"
 }
+
+def waitForDeployment(String deploymentName,String namespace,String timeoutInSeconds)
+{
+    sh "oc wait --for=condition=Available deployment/${deploymentName} --timeout=${timeoutInSeconds}s -n ${namespace}"
+
+}
